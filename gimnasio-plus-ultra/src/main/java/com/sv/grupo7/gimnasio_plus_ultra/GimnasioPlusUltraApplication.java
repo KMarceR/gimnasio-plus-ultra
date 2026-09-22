eeb8352009964cd65faff1dbbff0afc1936d2af3
KMarceR <kmarcebr@gmail.com>
2026-09-21 22:05:04 -0600

    Se agrego el backend inicial

---
+++ gimnasio-plus-ultra/src/main/java/com/sv/grupo7/gimnasio_plus_ultra/GimnasioPlusUltraApplication.java
@@ -0,0 +1,13 @@
+package com.sv.grupo7.gimnasio_plus_ultra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimnasioPlusUltraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimnasioPlusUltraApplication.class, args);
+	}
+
+}
---
+++ gimnasio-plus-ultra/src/test/java/com/sv/grupo7/gimnasio_plus_ultra/GimnasioPlusUltraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.grupo7.gimnasio_plus_ultra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioPlusUltraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
